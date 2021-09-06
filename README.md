c5767d1cee4544c5dfcfd06b7d84123b5f97c517
Vojtech Cerveny <cervik49@gmail.com>
2021-09-06 23:19:53 +0200

    first batch of homeworks

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # labs-01
-Labs 01
+All files should return results into console. Don't forget call functions.
+
+Note: Don't forget comment your code. It is better to read why you implemented solution like you did. 
---
+++ array.js
@@ -0,0 +1,28 @@
+// this block generates array with random length with values between 1-100
+let numbers = [];
+let length = Math.ceil(Math.random() * 10) + 5
+for (let i = 0; i < length; i = i + 1) {
+  numbers.push(Math.ceil(Math.random() * 100))
+}
+
+// EXERCISE: 
+// Display: 
+// a) array
+// b) length of array 
+// c) first element 
+// d) last element 
+// e) largest number 
+// f) smallest number 
+// g) sum of all numbers in array, 
+// h) difference between the largest and the smallest number 
+// i) average of all numbers 
+// j) index of largest 
+// k) display even numbers, if array doesn't contain even number, show text "Even number isn't in array"  
+// l) Multiple 2 every number in array ->  [1,2,3] -> [2,4,6]  
+// m) If array contains number smaller than 10, say "Wow, this {smallest number} is very small."  
+// n) Create new array with duplicities of numbers -> [1,2,3] -> [1,1,2,2,3,3]  
+// o) Create two arrays - one with even numbers, second with odd numbers 
+// p) Display everything with descriptions in terminal - 
+
+// Your code: 
+
